c2d0767533762d03160182a32836d03e87a06f3b
MeowCanCode <46248187+MwanikiNjagi@users.noreply.github.com>
2024-03-05 16:09:17 +0300

    Add files via upload

--- intrinsics_vec_summation.c
+++ intrinsics_vec_summation.c
@@ -3,11 +3,10 @@
 #include <xmmintrin.h>
 #include <emmintrin.h>
 
-void intrinsics_sum(){
-    //using _mm_load_ps
-    float a[4] = {4.0, 2.3, 5.6, 7.} ;
-    float b[4] = {5.6, 7.0, 8.5, 6.6};
 
+
+void intrinsics_sum(float* a, float* b){
+    //using _mm_load_ps
     __m128 arr_a =  _mm_load_ps(a);
     __m128 arr_b =  _mm_load_ps(b);
 
@@ -16,20 +15,50 @@ void intrinsics_sum(){
     __m128 arr_b = _mm_set_ps(5.6, 7.0, 8.5, 6.6);
 */   
 
-    __m128 val_c = _mm_add_ps(arr_a, arr_b);
+    __m128 arr_c = _mm_add_ps(arr_a, arr_b);
 
-    float d[4];
+    float add_arr[4];
 
-    _mm_storeu_ps(d, val_c);
-    
-    for(int i = 0; i < sizeof(d)/sizeof(d[0]); i++){
-        printf("%f\n",d[i]);
+    _mm_storeu_ps(add_arr, arr_c);
+
+
+    for(int i = 0; i < sizeof(add_arr)/sizeof(add_arr[0]); i++){
+        printf("%f\n",add_arr[i]);
     };
+    
+    printf("..........\n");
+}
+
+void intrinsics_mul(float* a, float* b){
+    //loading the arrays
+    __m128 arr_a =  _mm_load_ps(a);
+    __m128 arr_b =  _mm_load_ps(b);
+
+    //doing math operation
+    __m128 arr_c  = _mm_mul_ps(arr_a, arr_b);
+
+    //initializing an array to store this value
+    float mult_arr[4];
+
+    //transferring end result to above array
+    _mm_storeu_ps(mult_arr, arr_c);
+
+    for(int i = 0;i < sizeof(mult_arr)/sizeof(mult_arr[i]); i++){
+        printf("%f\n",mult_arr[i]);
+    };
+    
+    printf("..........\n");
 }
 
 
+
 int main(){
-   intrinsics_sum();
+   float first[4] = {4.0, 2.3, 5.6, 7.};
+   float second[4] = {5.6, 7.0, 8.5, 6.6};
+
+   intrinsics_sum(first, second);
+   intrinsics_mul(first, second);
+
    return 0;
 
 } 
